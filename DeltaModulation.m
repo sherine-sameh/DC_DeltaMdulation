3a5b3d601047634b39c54d61d79f45f4c91073d5
sherine sameh <sherine-sameh@hotmail.com>
2016-04-15 21:41:48 +0200

    add square and DC signals

--- DeltaModulation.m
+++ DeltaModulation.m
@@ -5,31 +5,59 @@ Fs = 50;
 Ts = 1/Fs;
 
 t=0:Ts:1;
-Input=sin(2*pi*t);
-%Input = rectpuls(t);
-%Input= ones(50);
-plot(Input)
+
+InputSin=sin(2*pi*t);
+plot(InputSin,'Red')
 hold on
-
 delta=2*pi*Ts;
-
-for n=1:length(Input)
+for n=1:length(InputSin)
     if n==1
-        e(n) = Input(n);
+        e(n) = InputSin(n);
         eq(n) = delta*sign(n);
-        mq(n) = eq(n);
+        mqSin(n) = eq(n);
     else 
-        e(n) = Input(n)-mq(n-1);
+        e(n) = InputSin(n)-mqSin(n-1);
         eq(n) = delta*sign(e(n));
-        mq(n) = mq(n-1)+eq(n);
+        mqSin(n) = mqSin(n-1)+eq(n);
     end
 
 end
-stairs(mq)
+stairs(mqSin)
 
-for m=1:length(Input)
-    
-end
 
+InputDC= ones(50);
 figure
-plot(DM)
+plot(InputDC,'Red')
+hold on
+for n=1:length(InputDC)
+    if n==1
+        e(n) = InputDC(n);
+        eq(n) = delta*sign(n);
+        mqDC(n) = eq(n);
+    else 
+        e(n) = InputDC(n)-mqDC(n-1);
+        eq(n) = delta*sign(e(n));
+        mqDC(n) = mqDC(n-1)+eq(n);
+    end
+
+end
+stairs(mqDC)
+
+
+InputSquare= rectpuls(t);
+figure
+plot(InputSquare,'Red')
+hold on
+for n=1:length(InputSquare)
+    if n==1
+        e(n) = InputSquare(n);
+        eq(n) = delta*sign(n);
+        mqRect(n) = eq(n);
+    else 
+        e(n) = InputSquare(n)-mqRect(n-1);
+        eq(n) = delta*sign(e(n));
+        mqRect(n) = mqRect(n-1)+eq(n);
+    end
+
+end
+stairs(mqRect)
